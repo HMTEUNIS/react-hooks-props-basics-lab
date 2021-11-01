60f88f1b0c9ce7b77d0eb1ca0f3c2fe3e9c11622
Holly Teunis <+>
2021-11-01 17:51:03 -0400

    tryina get this p right

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  if (props.bio === "") {
+
+  <div id="about">
+    <h2>About Me</h2>
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    <a href="https://github.com/liza">{props.gitHub}</a>
+    <a href="https://github.com/liza">{props.linkedIn}</a>
+  </div>
+}
+  else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href="https://github.com/liza">{props.gitHub}</a>
+      <a href="https://github.com/liza">{props.linkedIn}</a>
     </div>
   );
-}
+}}
 
 export default About;
